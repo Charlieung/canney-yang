2dc60f44b5ca92c70947fe1bd68ee41ee3db23b8
charlieung <leung.charles@outlook.com>
2025-06-11 14:33:14 -0400

    feat: convert to astro

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Agency v7.0.7 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
